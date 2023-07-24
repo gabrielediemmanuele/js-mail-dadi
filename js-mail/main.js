a309f0c2d21547037c476a4a9365d95e5decbbaa
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-24 15:17:38 +0200

    feat:  add cycle and condition

--- js-mail/main.js
+++ js-mail/main.js
@@ -1,3 +1,25 @@
 /* Chiedere la mail all'untete */
-const userMail = prompt("La tua E-Mail");
+let userMail = prompt("La tua E-Mail");
 console.log(userMail);
+
+/* Elenco delle mail valide */
+const emailsList = [
+  "bilbobeggins@rings.it",
+  "harrypotter@hogwarts.com",
+  "gabriele.diemmmanuele@outlook.it",
+];
+
+/* Esito  della risposta */
+let outcomeLog = document.getElementById("outcome");
+
+/* Esito Negativo */
+outcomeLog.innerHTML = "Mi dispiace, la tua mail non è presente nel database!";
+
+/* Controllo elenco mail */
+for (let i = 0; i < emailsList.length; i++) {
+  if (userMail === emailsList[i]) {
+    // e se la mail inserita è corretta....
+    let emailOk = "L'indirizzo e-mail inserito è corretto!";
+    outcomeLog.innerHTML = emailOk;
+  }
+}
