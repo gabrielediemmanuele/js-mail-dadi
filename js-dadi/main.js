1be32b2a00910e8896e9cd7917ab288e9e2c0507
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-24 15:42:02 +0200

    feat: add game conditions

--- js-dadi/main.js
+++ js-dadi/main.js
@@ -15,3 +15,13 @@ pc = Math.floor(Math.random() * 6 + 1);
 console.log(player, megatron);
 
 //Determinare il vincitore in base al numero
+
+document.getElementById("dice-game").innerHTML = messageAllert;
+
+messageAllert = "è un pareggio!";
+
+if (player > megatron) {
+  messageAllert = "hai vinto !";
+} else if (player < megatron) {
+  message = "Il tuo pianeta verrà distrutto!";
+}
